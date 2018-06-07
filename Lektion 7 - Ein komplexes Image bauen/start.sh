d95eb85566d6f965433643c40b04a4973000bd3c
Frank Geisler <frank_geisler@geislers.net>
2018-06-07 08:07:29 +0200

    Zusatzdateien hinzugefügt

---
+++ Lektion 7 - Ein komplexes Image bauen/start.sh
@@ -0,0 +1,13 @@
+version: '2'
+services:
+zoo1:
+        image: 31z4/zookeeper
+        restart: always
+        ports:
+            - 2181:2181
+        environment:
+            ZOO_MY_ID: 1
+            ZOO_SERVERS: server.1=zoo1:2888:3888 server.2=zoo2:2888:3888 server.3=zoo3:2888:3888
+
+    zoo2:
+        image: 31z4/zookeeper
